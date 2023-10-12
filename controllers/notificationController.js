6b81767a0c036ff2fd859af3fc6c2f2914f6c1f2
Anand Patel <anandrajpatel134@gmail.com>
2023-10-12 10:19:19 +0530

    Bug Fix

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -7,7 +7,7 @@ function sendIndividualPushNotification({ title, body, token }) {
       body: body,
     },
     token:
-      "dU_oI_R2TSGsMOwy7ZsISY:APA91bHHY7EQut2S7qDEVZz6dtObCo3vCTN_HhvO9zH6ykEtAJNgh7oDvakea25LjXmt5CFfiZ11l8WcXCSBuqbs-1yHkICyfHUV-KvsZulUaz1OLygTpd0osvdYLnMflGEUpNdMofVD",
+      token,
   };
   admin
     .messaging()
